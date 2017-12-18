0fc8bf1d784373ca085fd7bb5e81c5222f16d6ed
m <1459449180@qq.com>
2017-12-18 15:54:24 +0800

    hello

---
+++ app/src/main/java/a01/jiyun/com/android_day01_01/MainActivity.java
@@ -0,0 +1,13 @@
+package a01.jiyun.com.android_day01_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
